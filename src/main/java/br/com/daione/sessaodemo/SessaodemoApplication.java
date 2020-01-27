d9609e7c6c4d3886a17fde8bc97494633cf61182
daione_pavan <daione.pavan@compasso.com.br>
2020-01-27 14:11:30 -0300

    sessao demo

---
+++ src/main/java/br/com/daione/sessaodemo/SessaodemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.daione.sessaodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessaodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessaodemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/daione/sessaodemo/controllers/MainController.java
@@ -0,0 +1,13 @@
+package br.com.daione.sessaodemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping
+    public String teste(){
+        return "teste";
+    }
+}
